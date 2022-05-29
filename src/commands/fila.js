60d23d0bcb99424903faddd6a974e58b970fefea
Marcelo <marcelo.maga2550@gmail.com>
2022-05-29 16:45:51 -0300

    Fila
    Arquivo experimental

---
+++ src/commands/fila.js
@@ -0,0 +1,7 @@
+const Discord = require('discord.js')
+const { queueName } = require('../queueFile')
+
+
+module.exports.run = async (client, message, args) => {
+  return message.channel.send(``)
+}
